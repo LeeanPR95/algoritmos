7b39c0ad555114e7b93bc0f7f03826b6ccad45cb
LeeanPR95 <oliveralean@gmail.com>
2024-03-08 16:02:25 -0300

    Apenitas comenzando con algo básico

---
+++ pdf-1/ej1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ejercicio 1
+	// realizar un algoritmo que imprima los numeros del 1 al 100
+	// usar bucles
+
+	c := 0
+	for c < 10+1 {
+		fmt.Print(c, " ")
+		c++
+	}
+}
