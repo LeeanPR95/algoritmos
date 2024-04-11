da024c4debb538492d08487a74cafc46ec8fe78b
LeeanPR95 <oliveralean@gmail.com>
2024-04-10 23:50:52 -0300

    Un if else básico básico

--- pdf_2/par_impar/main.go
+++ pdf_2/par_impar/main.go
@@ -1,12 +1,24 @@
 package main
 
-//import (
-//	"fmt"
-//)
+import (
+	"fmt"
+)
 
 func main() {
 	/* Elabora un algoritmo para leer un número 
 	   y determinar si éste es par o impar
 	*/
+
+	// variables
+
+	var num int
+	fmt.Printf("Ingrese el numero \n")
+	fmt.Scanln(&num)
+
+	if num % 2 == 0 {
+		fmt.Printf("%d es un número par", num)
+	} else {
+		fmt.Printf("%d es un número impar", num)
+	}
 	
 }
